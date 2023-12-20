22a7d547476ca747d74e7e6b0a81be0164a8dedf
Raul Albín <raul200794@gmail.com>
2023-12-20 18:24:18 +0100

    normalize appbar from resume screen

--- lib/resume/widgets/appbar.dart
+++ lib/resume/widgets/appbar.dart
@@ -1,4 +1,3 @@
-
 import 'package:dribbble_financial/core/extensions/context_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -10,25 +9,31 @@ class Appbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Gap(kToolbarHeight),
-        Row(
-          children: [
-            const Gap(30.0),
-            Text(
-              'Dashboard',
-              style: context.theme.titleLarge,
-            ),
-            const Gap(60),
-            const Icon(Icons.notifications_none_rounded),
-            const Gap(40.0),
-            const Icon(Icons.toc_outlined)
-          ],
-        ),
-        const Gap(kToolbarHeight - 10.0),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        children: [
+          const Gap(kToolbarHeight),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+            
+              Text(
+                'Dashboard',
+                style: context.theme.titleLarge,
+              ),
+              const Row(
+                children: [
+                  Icon(Icons.notifications_none_rounded),
+                  Gap(40.0),
+                  Icon(Icons.toc_outlined)
+                ],
+              )
+            ],
+          ),
+          const Gap(kToolbarHeight - 10.0),
+        ],
+      ),
     );
   }
 }
-
